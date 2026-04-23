56384bf752a6f32fcc8c0cf05eff3f1e7d10c55b
Luís Guilherme de S Lopes <luisgslopes2@gmail.com>
2026-04-23 17:05:33 -0300

    v26

---
+++ src/model/Usuario.java
@@ -0,0 +1,62 @@
+package model;
+public class Usuario {
+    private int id;
+    private String nome, sobrenome, email, cidade, cpf;
+
+    public Usuario(int id, String nome, String sobrenome, String email, String cidade, String cpf) {
+        this.id = id;
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.email = email;
+        this.cidade = cidade;
+        this.cpf = cpf;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+}
